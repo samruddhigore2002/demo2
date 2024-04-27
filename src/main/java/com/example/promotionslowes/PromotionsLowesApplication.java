d5059959b2374738ad99fe70e9256723efeefb76
Samruddhi Gore <samruddhi.gore@cumminscollege.in>
2024-04-27 12:40:47 +0530

    initial commit

---
+++ src/main/java/com/example/promotionslowes/PromotionsLowesApplication.java
@@ -0,0 +1,13 @@
+package com.example.promotionslowes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionsLowesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionsLowesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/promotionslowes/PromotionsLowesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotionslowes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionsLowesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
